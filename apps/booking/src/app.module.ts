32954069e8a2d337961a1327999b28aaa09b884c
Nodir <nodirhikmatov6gmail.com>
2025-05-21 00:30:06 +0900

    feat: developed mongodb

--- apps/booking/src/app.module.ts
+++ apps/booking/src/app.module.ts
@@ -1,11 +1,26 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
+import { ComponentsModule } from './components/components.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		GraphQLModule.forRoot({
+			driver: ApolloDriver,
+			playground: true,
+			uploads: false,
+			autoSchemaFile: true,
+		}),
+		ComponentsModule,
+		DatabaseModule,
+	],
+	controllers: [AppController],
+	providers: [AppService, AppResolver],
 })
 export class AppModule {}
