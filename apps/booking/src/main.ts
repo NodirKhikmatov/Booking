ba541c2359c753c8453834d6950aef4eeb9c0cde
Nodir <nodirhikmatov6gmail.com>
2025-06-02 19:45:09 +0900

    feat: standard integration of logging interceptores

--- apps/booking/src/main.ts
+++ apps/booking/src/main.ts
@@ -1,10 +1,12 @@
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './libs/interceptor/login.interceptor';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalInterceptors(new LoggingInterceptor());
 	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
