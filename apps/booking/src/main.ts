aa7e443f633c1214eb18c0f6739f3d60690a1c2c
Nodir <nodirhikmatov6gmail.com>
2025-06-03 14:23:33 +0900

    feat: modify upload images configuration

--- apps/booking/src/main.ts
+++ apps/booking/src/main.ts
@@ -1,12 +1,19 @@
+import * as express from 'express';
+
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './libs/interceptor/login.interceptor';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { graphqlUploadExpress } from 'graphql-upload';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
 	app.useGlobalInterceptors(new LoggingInterceptor());
+	app.enableCors({ origin: true, credentials: true });
+
+	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
+	app.use('/uploads', express.static('./uploads'));
 	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
