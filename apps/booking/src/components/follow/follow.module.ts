2e6d9170d80da0d6b72ca3375988a4a4a8fe9077
Nodir <nodirhikmatov6gmail.com>
2025-06-04 16:05:41 +0900

    feat: created follow related resolver and service

--- apps/booking/src/components/follow/follow.module.ts
+++ apps/booking/src/components/follow/follow.module.ts
@@ -1,4 +1,14 @@
+import { AuthModule } from '../auth/auth.module';
+import { FollowResolver } from './follow.resolver';
+import FollowSchema from '../../schemas/Follow.model';
+import { FollowService } from './follow.service';
+import { MemberModule } from '../member/member.module';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+	imports: [MongooseModule.forFeature([{ name: 'Follow', schema: FollowSchema }]), AuthModule, MemberModule],
+	providers: [FollowResolver, FollowService],
+	exports: [FollowService],
+})
 export class FollowModule {}
