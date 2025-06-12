90bccb93cd03a891fc3735b639c6139a97a1af6a
Nodir <nodirhikmatov6gmail.com>
2025-06-12 16:32:30 +0900

    feat: develop socket gatewayts

--- apps/booking/src/socket/socket.gateway.ts
+++ apps/booking/src/socket/socket.gateway.ts
@@ -1,22 +1,33 @@
+import * as url from 'url';
+
 import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, WebSocket } from 'ws';
 
+import { AuthService } from '../components/auth/auth.service';
 import { Logger } from '@nestjs/common';
+import { Member } from '../libs/dto/member/member';
 
 interface MessagePayload {
 	event: string;
 	text: string;
+	memberData: Member;
 }
 
 interface InfoPayLoad {
 	event: string;
 	totalClients: number;
+	memberData: Member;
+	action: string;
 }
 
 @WebSocketGateway({ transports: ['websocket'], secure: false })
 export class SocketGateway implements OnGatewayInit {
 	private logger: Logger = new Logger('SocketEventsGateway');
 	private summaryClient = 0;
+	private clientsAuthMap = new Map<WebSocket, Member>();
+	private messageList: MessagePayload[] = [];
+
+	constructor(private authService: AuthService) {}
 
 	@WebSocketServer()
 	server: Server;
@@ -25,33 +36,67 @@ export class SocketGateway implements OnGatewayInit {
 		this.logger.verbose(`WebSocket server initialized. Total clients: ${this.summaryClient}`);
 	}
 
-	handleConnection(client: WebSocket) {
-		this.summaryClient++;
-		this.logger.verbose(`Client connected. Total: ${this.summaryClient}`);
-
-		const infoMsg: InfoPayLoad = {
-			event: 'info',
-			totalClients: this.summaryClient,
-		};
-		this.emitMessage(infoMsg);
+	private async retriveAuth(req: any): Promise<Member | null> {
+		try {
+			const parseUrl = url.parse(req.url, true);
+			const { token } = parseUrl.query;
+			console.log('token', token);
+			return await this.authService.verifyToken(token as string);
+		} catch (err) {
+			return null;
+		}
 	}
 
-	handleDisconnect(client: WebSocket) {
-		this.summaryClient--;
-		this.logger.verbose(`Client disconnected. Total: ${this.summaryClient}`);
+	public async handleConnection(client: WebSocket, req: any) {
+		const authMember = await this.retriveAuth(req);
+		this.summaryClient++;
+		this.clientsAuthMap.set(client, authMember!);
+
+		const clientNick: string = authMember?.memberNick ?? 'Guest';
+		this.logger.verbose(`Client [${clientNick}] connected. Total: ${this.summaryClient}`);
 
 		const infoMsg: InfoPayLoad = {
 			event: 'info',
 			totalClients: this.summaryClient,
+			memberData: authMember!,
+			action: 'joined',
+		};
+		this.emitMessage(infoMsg);
+		client.send(JSON.stringify({ event: 'getMessages', list: this.messageList }));
+	}
+
+	public handleDisconnect(client: WebSocket) {
+		const authMember = this.clientsAuthMap.get(client);
+		this.summaryClient--;
+		this.clientsAuthMap.delete(client);
+
+		const clientNick: string = authMember?.memberNick ?? 'Guest';
+		this.logger.verbose(`Client [${clientNick}] disconnected. Total: ${this.summaryClient}`);
+
+		const infoMsg: InfoPayLoad = {
+			event: 'info',
+			totalClients: this.summaryClient,
+			memberData: authMember!,
+			action: 'left',
 		};
 		this.broadcastMessage(client, infoMsg);
 	}
 
 	@SubscribeMessage('message')
 	async handleMessage(client: WebSocket, payload: any): Promise<void> {
-		const newMessage: MessagePayload = { event: 'message', text: payload };
+		const authMember = this.clientsAuthMap.get(client); // ✅ Retrieve stored member
 
-		this.logger.verbose(`NEW MESSAGE: ${payload}`);
+		const newMessage: MessagePayload = {
+			event: 'message',
+			text: payload,
+			memberData: authMember!,
+		};
+		this.messageList.push();
+		if (this.messageList.length > 5) this.messageList.splice(0, this.messageList.length - 5);
+
+		const clientNick: string = authMember?.memberNick ?? 'Guest';
+
+		this.logger.verbose(`NEW MESSAGE: [${clientNick}] ${payload}`);
 		this.emitMessage(newMessage);
 	}
 
@@ -71,3 +116,9 @@ export class SocketGateway implements OnGatewayInit {
 		});
 	}
 }
+
+/*
+client only client
+brodcast except client 
+emit al clients
+*/
--- apps/booking/src/socket/socket.module.ts
+++ apps/booking/src/socket/socket.module.ts
@@ -1,7 +1,9 @@
+import { AuthModule } from '../components/auth/auth.module';
 import { Module } from '@nestjs/common';
 import { SocketGateway } from './socket.gateway';
 
 @Module({
 	providers: [SocketGateway],
+	imports: [AuthModule],
 })
 export class SocketModule {}
