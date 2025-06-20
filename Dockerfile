20e46d9abf75c07fa0226b9b230af02384a1f855
Nodir <nodirhikmatov6gmail.com>
2025-06-20 12:18:27 +0900

    feat: idk

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,22 @@
-FROM node:20.10.0
+FROM node:20-alpine AS builder
 
-WORKDIR /usr/src/app
+WORKDIR /app
 
 COPY package*.json ./
-RUN npm install
+RUN npm ci
 
 COPY . .
-
 RUN npm run build
 
-CMD ["npm", "run", "start:prod"]
+FROM node:20-alpine
+
+WORKDIR /app
+
+COPY --from=builder /app/package*.json ./
+COPY --from=builder /app/node_modules ./node_modules
+COPY --from=builder /app/dist ./dist
+
+ENV NODE_ENV=production
+EXPOSE 3000
+
+CMD ["node", "dist/apps/booking/main.js"]
--- apps/booking/src/main.ts
+++ apps/booking/src/main.ts
@@ -1,19 +1,21 @@
 import * as express from 'express';
 
+// @ts-ignore
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './libs/interceptor/login.interceptor';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { WsAdapter } from '@nestjs/platform-ws';
-import graphqlUploadExpress from 'graphql-upload'; // <-- default import here
+import { graphqlUploadExpress } from 'graphql-upload'; // <-- default import here
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
 	app.useGlobalInterceptors(new LoggingInterceptor());
 	app.enableCors({ origin: true, credentials: true });
-	// @ts-ignore
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
+
 	app.useWebSocketAdapter(new WsAdapter(app));
 	app.use('/uploads', express.static('./uploads'));
 	await app.listen(process.env.PORT_API ?? 3000, '0.0.0.0');
