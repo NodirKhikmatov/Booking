9bece0d94ee4bd2fa852283301c2a2102ce695f6
Nodir <nodirhikmatov6gmail.com>
2025-06-21 16:41:53 +0900

    fix:ed

--- apps/booking/src/app.module.ts
+++ apps/booking/src/app.module.ts
@@ -18,6 +18,7 @@ import { T } from './libs/types/common';
 			playground: true,
 			uploads: false,
 			autoSchemaFile: true,
+			introspection: true, // <-- Add this line
 			formatError: (error: T) => {
 				const graphQLFormattedError = {
 					code: error?.extensions.code,
